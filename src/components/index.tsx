dac0949242d37e5c879b44be0a7cb720ec5c744e
rivailJunior <rivail.rj@gmail.com>
2023-06-22 22:50:04 -0400

    feat: pagination test

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,4 +3,3 @@ export { Card } from "./cards/card";
 export { GridCards } from "./grid/grid";
 export { Pagination } from "./pagination/pagination";
 export { Header } from "./header/header";
-export { HorizontalScrollCards } from "./grid/horizontal";
---
+++ tests/unit/pagination.test.tsx
@@ -0,0 +1,22 @@
+import { render, screen } from "@testing-library/react";
+import { describe, expect, test } from "vitest";
+import { Pagination } from "y/components";
+
+describe("Pagination", () => {
+  test("should render pagination component properly", () => {
+    const paginationItems = 5;
+    render(
+      <Pagination
+        paginationItems={paginationItems}
+        offset={1}
+        limit={20}
+        totalPages={50}
+      />
+    );
+    for (let item = 0; item < paginationItems; item++) {
+      expect(screen.getByText(item)).toBeDefined();
+    }
+    expect(screen.getByText(/first/i)).toBeDefined();
+    expect(screen.queryByText(/last/i)).toBeFalsy();
+  });
+});
